f83311e8d74e1112a02378dd07a268c3ab77bebc
Giulia Straccali <straccaligiulia@gmail.com>
2020-12-17 11:47:07 +0100

    Aggiungere i file di progetto.

---
+++ NumeriNegativi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumeriNegativi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            Console.WriteLine(a);
+            Console.WriteLine(a + 1);
+            
+        }
+    }
+}
